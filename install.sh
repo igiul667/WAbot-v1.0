9ab4141bf4149284de0d38c8faee87b9e752c17d
igiul667 <48596120+igiul667@users.noreply.github.com>
2021-04-09 11:36:28 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,35 +10,42 @@ else
   exit 9
 fi
 echo "Installing/checking node.js"
-{ node --version && echo "Node is already installed"} ||  #try
-{ #catch
+node --version
+if $? -eq 0; then
+    echo "Node is already installed"
+else
     echo "Node not installed, installing..."
-    {apt-get install nodejs npm -y && echo "Succesfully installed Node.js and NPM"} ||
-    {  
+    apt-get install nodejs npm -y 
+    if $? -eq 0; then
+        echo "Succesfully installed Node.js and NPM"
+    else
       echo "Error installing Node.js or NPM"
       exit 8
-    }
-  }
+    fi
+fi
 
 echo "Installing/updating node modules"
-{ pm install --update && echo "Sucesfully installed Node.js packages"} ||  #try
-{ #catch
+pm install --update 
+if $? -eq 0; then
+    echo "Sucesfully installed Node.js packages"
+else
     echo "Error installing Node.js packages"
-    exit 7 
-}
+    exit 7
+fi
 echo "Installation complete, starting configuration"
 dir_path="$(dirname $(realpath $0))/WAbot"
 echo "Configuring work dir to:$dir_path"
 read -p "Insert language for responses and TTS:" lanSet
 if [ -f "./$lanSet.lan" ]; then #check if selected lagnuage file exhists
     echo "Sucesfully selected language: $lanSet"
-else 
+else
     echo "Language selected is not valid, aborting"
     exit 6
 fi
 printf "%s\n%s\npython3 " > "./WAbot/setting.set"
 echo "Configuration complete"
 exit 0
+
 #ffmpeg system install
 #windows-curses #for windows systems
 
