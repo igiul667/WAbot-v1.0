53e8ab8ca60700cff573aa7b7f512049944c2886
igiul667 <igiuldj@gmail.com>
2021-04-09 16:33:03 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 echo "This file will install WAbot version: "
 echo "Installing/checking python libraries..."
-pip3 install youtube-search-python pytube pydub Google-Images-Search gtts
+pip3 install youtube-search-python pytube pydub Google-Images-Search gtts  > /dev/null
 echo "Installing/checking FFmpeg"
-if apt-get install ffmpeg -y; then
+if sudo apt-get install ffmpeg -y > /dev/null; then
   echo "Succesfully installed FFmpeg"
 else
   echo "Error installing FFmpeg"
@@ -15,7 +15,7 @@ if [ $? -eq 0 ]; then
     echo "Node is already installed"
 else
     echo "Node not installed, installing..."
-    apt-get install nodejs npm -y 
+    sudo apt-get install nodejs npm -y > /dev/null
     if [ $? -eq 0 ]; then
         echo "Succesfully installed Node.js and NPM"
     else
@@ -25,7 +25,7 @@ else
 fi
 
 echo "Installing/updating node modules"
-npm install  mime-types venom-bot node-emoji
+npm install  mime-types venom-bot node-emoji  > /dev/null
 if [ $? -eq 0 ]; then
     echo "Sucesfully installed Node.js packages"
 else
@@ -33,10 +33,10 @@ else
     exit 7
 fi
 echo "Installation complete, starting configuration"
-dir_path="$(dirname $(realpath $0))/WAbot"
+dir_path="$(dirname $(realpath $0))/wabot"
 echo "Configuring work dir to:$dir_path"
 read -p "Insert language for responses and TTS:" lanSet
-if [ -f "./WAbot/languages/$lanSet.lan" ]; then #check if selected lagnuage file exhists
+if [ -f "./wabot/languages/$lanSet.lan" ]; then #check if selected lagnuage file exhists
     echo "Sucesfully selected language: $lanSet"
 else
     echo "Language selected is not valid, aborting"
@@ -50,4 +50,3 @@ exit 0
 
 #ffmpeg system install
 #windows-curses #for windows systems
-
