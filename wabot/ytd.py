3d8a7c65d6d952a453ae0b9ce824c94c439cc487
igiul667 <48596120+igiul667@users.noreply.github.com>
2021-05-30 15:53:33 +0200

    Update ytd.py

--- wabot/ytd.py
+++ wabot/ytd.py
@@ -1,8 +1,7 @@
 #fetch video and audio from youtube
 import argparse
+import youtube_dl
 from youtubesearchpython import VideosSearch
-from pytube import YouTube
-from pydub import AudioSegment
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-t","--titolo",type=str)
@@ -12,20 +11,31 @@ args = parser.parse_args()
 
 if "https://" in args.titolo:
     try:
-        ytObj = YouTube(args.titolo.replace(" ",""))
+        link = args.titolo
     except:
-       print("error general")
+        print("error general")
 else:
     try:
         videosSearch = VideosSearch(args.titolo, limit = 1)
-        ytObj = YouTube(videosSearch.result().get("result")[0]["link"])
+        link = videosSearch.result().get("result")[0]["link"]
     except:
         print("error searching video")
-try:
-    if args.mode == "video":
-        ytObj.streams.filter(progressive=True, file_extension="mp4").get_highest_resolution().download(filename=args.name)
-    else:
-        ytObj.streams.filter(only_audio=True).first().download(filename=args.name)
-        AudioSegment.from_file(args.name+".mp4", "mp4").export(args.name+".mp3",format="mp3")
-except:
-    print("error downloading")
+if args.mode == "video":
+    ydl_opts = {
+         'format': 'best[filesize<64M][ext=mp4]',
+         'outtmpl': args.name+'.%(ext)s',
+     }
+else:
+     ydl_opts = {
+         'format': 'best[filesize<64M]',
+         'outtmpl': args.name+'.%(ext)s',
+         'postprocessors': [{
+             'key': 'FFmpegExtractAudio',
+             'preferredcodec': 'mp3',
+             'preferredquality': '320',
+         }],
+     }
+
+with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+    ydl.download([link])
+
